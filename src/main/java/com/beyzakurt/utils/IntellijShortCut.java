0b05e379c64792dea1faea2af10f660e3152db38
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-16 14:00:41 +0300

    Shortcuts

---
+++ src/main/java/com/beyzakurt/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.beyzakurt.utils;
+
+public class IntellijShortCut {
+    // Settings: ctrl+alt+s
+    // Constructor, getter, setter: alt+insert
+    // Single comment: ctrl+/
+    // Multiple comment: ctrl+shift+/
+}
