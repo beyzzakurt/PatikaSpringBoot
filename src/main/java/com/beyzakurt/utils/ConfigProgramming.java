c4b6e7de843146fdf1df478f467f71dad1a8b431
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-16 14:01:46 +0300

    DevTools Activate

---
+++ src/main/java/com/beyzakurt/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.beyzakurt.utils;
+
+public class ConfigProgramming {
+    // DevTools: LiveReload
+    // File => Settings => Build =>  Compiler => Build project automatically
+    // File => Settings => Advanced Setting => Allow auto make to start
+}
